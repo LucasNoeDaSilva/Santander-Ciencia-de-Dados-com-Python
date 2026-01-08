e05f21a1e7ef05a3f0d284d8bd387388127120a9
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-01-08 19:39:16 -0300

    docs: refatorando o nomes dos arquivos

---
+++ IntroducaoaPythonEGithub/02tipo_de_dados.py
@@ -0,0 +1,7 @@
+nome = str("Lucas Noe da Silva")
+idade = int(24)
+verificacao = True
+
+print("me chamo: ", nome)
+print("minha idade é: ", idade)
+print("e sou brasileiro", verificacao)
---
+++ IntroducaoaPythonEGithub/04conversao_de_tipos.py
@@ -0,0 +1,13 @@
+valor = 10
+print(valor)
+
+valor = float(10)
+print(valor)
+
+valor2 = "10.5"
+print(valor2)
+valor3 =int(10.5)
+print(valor3)
+
+print(int("40"))
+print(type(valor))
