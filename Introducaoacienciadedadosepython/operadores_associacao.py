4c4e89328566b149b7d5a1c2acf7b5953341d1c0
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-01-11 00:25:18 -0300

    Featch: trabalhando com operadores de associação

---
+++ Introducaoacienciadedadosepython/operadores_associacao.py
@@ -0,0 +1,8 @@
+nome = "lucas"
+linguagens = ["java", "python", "c+"]
+valores = [100,200,300]
+# in verifica se tem na lista
+
+print("java" in linguagens)
+print("java" not in linguagens)
+print(100 in valores)
