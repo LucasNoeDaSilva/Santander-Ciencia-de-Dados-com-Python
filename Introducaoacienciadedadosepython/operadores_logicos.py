eb8260eb4631b250ddb740f8a582e868ff5c88ad
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-01-11 00:01:38 -0300

    Featch: trablhando com operadores logicos

---
+++ Introducaoacienciadedadosepython/operadores_logicos.py
@@ -0,0 +1,12 @@
+saldo = 1000
+saque = 200
+limite_saque = 100
+
+#and = e
+print(saldo > saque and limite_saque >= saque)
+
+#or = ou
+print(saldo > saque or limite_saque >= saque)
+
+#not = negar
+print(not saldo > saque)
