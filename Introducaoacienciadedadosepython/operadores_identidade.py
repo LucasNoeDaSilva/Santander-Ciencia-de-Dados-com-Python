ba1cbeb4ca4febc7bed76be9ea6ad1cbcfccd7b5
Lucas Noe da Silva <lucasnoedasilva@icloud.com>
2026-01-11 00:19:45 -0300

    Featch: trablhando com operadores de identidade

---
+++ Introducaoacienciadedadosepython/operadores_identidade.py
@@ -0,0 +1,10 @@
+nome = "Luca Noe"
+nome_completo = nome
+saldo=100
+limite =100
+
+#is compara na memoria os valores
+print(nome is nome_completo)
+
+print(nome is not nome_completo)
+print(saldo is limite)
